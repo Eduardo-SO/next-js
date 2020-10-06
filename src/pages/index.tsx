7904b24b77bb2db639791471c30ddfdb7923ded0
Eduardo SO <eduardo.sool07@gmail.com>
2020-10-05 23:52:42 -0300

    Get products from the API using SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import { GetServerSideProps } from 'next';
+
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProducts {
+  id: number;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProducts[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello world</Title>
+      <Title>Produtos recomendados</Title>
+      <ul>
+        {recommendedProducts.map(recommendedProduct => (
+          <li>
+            <strong>{recommendedProduct.id}</strong>
+            <span> {recommendedProduct.title}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
