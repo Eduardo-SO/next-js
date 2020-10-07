c1c7c5291dc3f5817375a79b59d7a31dad5bd981
Eduardo SO <eduardo.sool07@gmail.com>
2020-10-06 22:45:17 -0300

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from '../styles/pages';
+
+export default function NotFound() {
+  return (
+    <Container>
+      <div>
+        <Title>Are you lost?</Title>
+        This page could not be found.
+      </div>
+    </Container>
+  )
+}
