d1ce730cdbd17d76142730ac2971cf468e572ac9
Eduardo SO <eduardo.sool07@gmail.com>
2020-10-06 22:05:07 -0300

    Add dynamic static page

--- src/pages/catalog/categories/[slug].tsx
+++ src/pages/catalog/categories/[slug].tsx
@@ -1,9 +1,63 @@
-import { useRouter } from 'next/router'
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
-export default function Category() {
+import { Container, Title } from '../../../styles/pages';
+
+interface IProduct {
+  id: number;
+  title: string;
+}
+
+interface CategoryProps {
+  products: IProduct[];
+}
+
+export default function Category({ products }: CategoryProps) {
   const route = useRouter();
   
   return (
-    <h1>{route.query.slug}</h1>
+    <Container>
+      <div>
+        <Title>{route.query.slug}</Title>
+
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              <strong>{product.id}</strong>
+              <span> {product.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch(`http://localhost:3333/categories`);
+  const categories = await response.json();
+
+  const paths = categories.map((category: IProduct) => {
+    return {
+      params: { slug: category.id }
+    }
+  })
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async (context) => {
+  const { slug } = context.params;
+
+  const response = await fetch(`http://localhost:3333/products?category_id=${slug}`);
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60,
+  }
 }
---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+export default function Product() {
+  const route = useRouter();
+  
+  return (
+    <h1>{route.query.slug}</h1>
+  )
+}
