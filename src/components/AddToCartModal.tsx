0b3d177eca079b01469ab8db834632960977b05d
Eduardo SO <eduardo.sool07@gmail.com>
2020-10-08 23:43:16 -0300

    Add lazy load to modal component

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,7 @@
+export default function AddToCartModal () {
+  return (
+    <h1>
+      Do you really want to add this product to cart?
+    </h1>
+  )
+}
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,9 +1,29 @@
-import { useRouter } from 'next/router'
+import { useCallback, useState } from 'react';;
+import { useRouter } from 'next/router';
+import dynamic  from 'next/dynamic';
+
+import { Container, Title, Button } from '../../../styles/pages';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Carregando...</p>, ssr: false }
+)
 
 export default function Product() {
   const route = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
   
+  const handleAddToCart = useCallback(() => {
+    setIsAddToCartModalVisible(!isAddToCartModalVisible);
+  }, []);
+
   return (
-    <h1>{route.query.slug}</h1>
+    <Container>
+      <Title>{route.query.slug}</Title>
+
+      <Button onClick={handleAddToCart}>Add to cart</Button>
+
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </Container>
   )
 }
